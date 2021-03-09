c86ea3cb82f081ebd1d689bfc2ec332d16cbfaa9
Wojciech Dybikowski <dybik08@gmail.com>
2021-03-09 18:42:00 +0100

    create repositories reducer

---
+++ src/__tests__/reducers/repositoriesReducer.test.ts
@@ -0,0 +1,50 @@
+import { repositories, repositoriesReducerInitialState } from '../../reducers/repositoriesReducer';
+import { NetworkActionNames } from '../../actions/networkActions';
+import mockedRepository from '../../__mocks__/mockRepositoryData';
+
+describe('repositories reducer tests', () => {
+    it('should return initial state', () => {
+        expect(
+            repositories(undefined, {
+                type: 'NON_EXISTING_ACTION',
+                payload: undefined,
+            })
+        ).toEqual(repositoriesReducerInitialState);
+    });
+
+    it('should handle START_FETCHING_REPOS action', () => {
+        expect(
+            repositories(repositoriesReducerInitialState, {
+                type: NetworkActionNames.START_FETCHING_REPOS,
+                payload: undefined,
+            })
+        ).toEqual({
+            ...repositoriesReducerInitialState,
+            loading: true,
+        });
+    });
+
+    it('should handle FETCHING_REPOS_ERROR action', () => {
+        expect(
+            repositories(repositoriesReducerInitialState, {
+                type: NetworkActionNames.FETCHING_REPOS_ERROR,
+                payload: undefined,
+            })
+        ).toEqual({
+            ...repositoriesReducerInitialState,
+            loading: false,
+        });
+    });
+
+    it('should handle FETCHING_REPOS_DONE action', () => {
+        expect(
+            repositories(repositoriesReducerInitialState, {
+                type: NetworkActionNames.FETCHING_REPOS_DONE,
+                payload: [mockedRepository],
+            })
+        ).toStrictEqual({
+            ...repositoriesReducerInitialState,
+            repos: [{ ...mockedRepository, key: '0', login: mockedRepository.owner.login }],
+        });
+    });
+});
--- src/reducers/repositoriesReducer.ts
+++ src/reducers/repositoriesReducer.ts
@@ -1,11 +1,38 @@
 import { NetworkActionNames } from '../actions/networkActions';
 import { Repository } from '../constants/types';
-const initialState = {
+
+export const repositoriesReducerInitialState = {
     repos: [],
     loading: false,
 };
 
-export const repositories = (state = initialState, action: { type: string; payload: any }) => {
+export const repositoriesResponseHandler = (repositories: Repository[]): Repository[] => {
+    return repositories.map((repo: Repository, index: number) => {
+        return {
+            key: `${index}`,
+            login: repo.owner.login,
+            language: repo.language,
+            stargazers_count: repo.stargazers_count,
+            name: repo.name,
+            owner: repo.owner,
+            created_at: repo.created_at,
+            updated_at: repo.updated_at,
+            license: repo.license,
+            description: repo.description,
+            forks: repo.forks,
+        };
+    });
+};
+
+interface RepositoriesReducer {
+    repos: Repository[];
+    loading: boolean;
+}
+
+export const repositories = (
+    state: RepositoriesReducer = repositoriesReducerInitialState,
+    action: { type: string; payload: any }
+): RepositoriesReducer => {
     switch (action.type) {
         case NetworkActionNames.FETCH_REPOS:
             return state;
@@ -24,21 +51,7 @@ export const repositories = (state = initialState, action: { type: string; paylo
         case NetworkActionNames.FETCHING_REPOS_DONE:
             return {
                 ...state,
-                repos: action.payload.map((repo: Repository, index: number) => {
-                    return {
-                        key: `${index}`,
-                        login: repo.owner.login,
-                        language: repo.language,
-                        stargazers_count: repo.stargazers_count,
-                        name: repo.name,
-                        owner: repo.owner,
-                        created_at: repo.created_at,
-                        updated_at: repo.updated_at,
-                        license: repo.license,
-                        description: repo.description,
-                        forks: repo.forks,
-                    };
-                }),
+                repos: repositoriesResponseHandler(action.payload),
                 loading: false,
             };
         default:
